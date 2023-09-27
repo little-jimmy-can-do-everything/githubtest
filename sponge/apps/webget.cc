f07ac980084f7728f15738a4a2c2fc7e19aa5ea9
little-jimmy-can-do-everything <2489400982@qq.com>
2023-09-27 23:13:44 +0800

    first commit

---
+++ sponge/apps/webget.cc
@@ -0,0 +1,57 @@
+#include "socket.hh"
+#include "util.hh"
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void get_URL(const string &host, const string &path) {
+    Address addr(host, "http");
+    TCPSocket sock;
+    sock.connect(addr);
+    sock.write("GET ");
+    sock.write(path);
+    sock.write(" HTTP/1.1\r\n");
+    sock.write("Host: ");
+    sock.write(host);
+    sock.write("\r\n\r\n");
+    sock.shutdown(SHUT_WR);
+
+    while (!sock.eof()){
+	    string ret = sock.read();
+	    cout<<ret;
+    }
+
+    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    cerr << "Warning: get_URL() has not been implemented yet.\n";
+}
+
+int main(int argc, char *argv[]) {
+    try {
+        if (argc <= 0) {
+            abort();  // For sticklers: don't try to access argv[0] if argc <= 0.
+        }
+
+        // The program takes two command-line arguments: the hostname and "path" part of the URL.
+        // Print the usage message unless there are these two arguments (plus the program name
+        // itself, so arg count = 3 in total).
+        if (argc != 3) {
+            cerr << "Usage: " << argv[0] << " HOST PATH\n";
+            cerr << "\tExample: " << argv[0] << " stanford.edu /class/cs144\n";
+            return EXIT_FAILURE;
+        }
+
+        // Get the command-line arguments.
+        const string host = argv[1];
+        const string path = argv[2];
+
+        // Call the student-written function.
+        get_URL(host, path);
+    } catch (const exception &e) {
+        cerr << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
